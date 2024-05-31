0f905606e3e50325894d7dc52dd96595de2237d4
Simon <s.elkhoury@zeroandone.me>
2024-05-31 13:01:18 +0300

    feat : upload policy file test

--- modules/artifacts_bucket.tf
+++ modules/artifacts_bucket.tf
@@ -1,6 +1,8 @@
 locals {
   principal_policy     = var.principal_policy == "" ? "${path.module}/fixtures/policies/principal_policy.tmpl" : var.principal_policy
   artifact_bucket_name = "${local.account_id}-dce-artifacts-${var.namespace}"
+  bluepi_policies_object = "bluepi_policies"
+  bluepi_policies_path = "${path.module}/fixtures/bluepi_policies"
 }
 
 
@@ -63,6 +65,13 @@ resource "aws_s3_bucket_object" "principal_policy" {
   key    = "fixtures/policies/principal_policy.tmpl"
   source = local.principal_policy
   etag   = filemd5(local.principal_policy)
+}
+
+resource "aws_s3_bucket_object" "bluepi_policy_basic" {
+  bucket = aws_s3_bucket.artifacts.id
+  key    = "${local.bluepi_policies_object}/bluepi_policy_basic.tmpl"
+  source = "${local.bluepi_policies_path}/bluepi_policy_basic.tmpl"
+  etag   = filemd5("${local.bluepi_policies_path}/bluepi_policy_basic.tmpl")
 }
 
 
