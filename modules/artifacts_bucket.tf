97841c53938aa1991f4704688cc8a5158ca4e7f8
unknown <s.elkhoury@zeroandone.me>
2025-05-12 12:39:22 +0300

    fix: wrong aws s3 resource

--- modules/artifacts_bucket.tf
+++ modules/artifacts_bucket.tf
@@ -81,7 +81,7 @@ resource "aws_s3_bucket_object" "bluepi_policy_serverless" {
   etag   = filemd5("${local.bluepi_policies_local_path}/bluepi_policy_serverless.tmpl")
 }
 
-resource "aws_s3_object" "bluepi_policy_admin" {
+resource "aws_s3_bucket_object" "bluepi_policy_admin" {
   bucket = aws_s3_bucket.artifacts.id
   key    = "${local.bluepi_policies_keys_prefix}/bluepi_policy_admin.tmpl"
   source = "${local.bluepi_policies_local_path}/bluepi_policy_admin.tmpl"
